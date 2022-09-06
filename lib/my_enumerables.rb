18a7bc58986c5ebf55f0c1b7a566037074304fa7
Gohar <goharayub2008@gmail.com>
2022-09-06 11:20:12 -0400

    Implement my_each method in Array class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,25 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+
+  def my_each
+    # nil -> nil
+    # applies a yield block on each item in the array
+    # Note: uses yield
+
+    # save current array length
+    array_length = self.length
+    # Counter to iterate through all the array items
+    item_counter = 0
+    # Goes through all array items
+    while item_counter < array_length
+      # Applies yield block to each array item
+      yield(self[item_counter])
+      # Move to next item
+      item_counter += 1
+    end
+    # Return array
+    self
+  end
+
 end
